5a067e93336b3abe006a9df05afd4163bc6c7a20
Blake Burch <blake@pmg.com>
2020-04-14 17:52:41 -0500

    90% functionality for Zip files

--- messaging/slack/send_slack_message.py
+++ messaging/slack/send_slack_message.py
@@ -3,6 +3,9 @@ import os
 import time
 import argparse
 import code
+import glob
+import re
+from zipfile import ZipFile
 
 
 def getArgs(args=None):
@@ -15,6 +18,8 @@ def getArgs(args=None):
     parser.add_argument('--users-to-notify',
                         dest='users_to_notify', required=False)
     parser.add_argument('--message', dest='message', required=True)
+    parser.add_argument('--source-file-name-match-type', dest='source_file_name_match_type',
+                        choices={'exact_match', 'regex_match'}, required=True)
     parser.add_argument('--source-file-name', dest='source_file_name',
                         required=False)
     parser.add_argument('--source-folder-name', dest='source_folder_name',
@@ -32,7 +37,7 @@ def send_slack_message(slack_connection, message, channel_name, blocks):
         channel=channel_name, link_names=True, text=message, blocks=blocks)
     print(
         f'Your message of "{message}" was successfully sent to {channel_name}')
-    return response
+    return message_response
 
 
 def upload_file_to_slack(slack_connection, file_name, channel_name, timestamp):
@@ -179,6 +184,75 @@ def update_slack_message(slack_connection, message, channel_id, blocks, timestam
     return message
 
 
+def find_all_local_file_names(source_folder_name):
+    """
+    Returns a list of all files that exist in the current working directory,
+    filtered by source_folder_name if provided.
+    """
+    cwd = os.getcwd()
+    cwd_extension = os.path.normpath(f'{cwd}/{source_folder_name}/*')
+    file_names = glob.glob(cwd_extension)
+    return file_names
+
+
+def find_all_file_matches(file_names, file_name_re):
+    """
+    Return a list of all file_names that matched the regular expression.
+    """
+    matching_file_names = []
+    for file in file_names:
+        if re.search(file_name_re, file):
+            matching_file_names.append(file)
+
+    return matching_file_names
+
+
+def combine_folder_and_file_name(folder_name, file_name):
+    """
+    Combine together the provided folder_name and file_name into one path variable.
+    """
+    combined_name = os.path.join(folder_name, file_name)
+    combined_name = os.path.normpath(combined_name)
+
+    return combined_name
+
+
+def compress_files(file_names):
+    print(f'{len(file_names)} files found. Compressing the files...')
+    with ZipFile('archive.zip', 'w') as zip_file:
+        for path in file_names:
+            zip_file.write(path)
+    print(
+        f'All {len(file_names)} files were successfully compressed into archive.zip')
+    return
+
+
+def is_too_large(file_path):
+    byte_max = 1000000000
+    if os.stat(file_path).st_size >= byte_max:
+        return True
+    else:
+        return False
+
+
+def determine_file_to_upload(source_file_name_match_type, source_folder_name, source_file_name):
+    if source_file_name_match_type == 'regex_match':
+        file_names = find_all_local_file_names(source_folder_name)
+        matching_file_names = find_all_file_matches(
+            file_names, re.compile(source_file_name))
+
+        compress_files(matching_file_names)
+        file_to_upload = 'archive.zip'
+    else:
+        if is_too_large(source_full_path):
+            print(f'{source_full_path} is too large. Compressing the file...')
+            compress_files([source_full_path])
+            file_to_upload = 'archive.zip'
+        else:
+            file_to_upload = source_full_path
+    return file_to_upload
+
+
 args = getArgs()
 destination_type = args.destination_type
 channel_name = args.channel_name
@@ -187,12 +261,14 @@ user_lookup_method = args.user_lookup_method
 users_to_notify = args.users_to_notify.lower()
 source_file_name = args.source_file_name
 source_folder_name = args.source_folder_name
+source_full_path = combine_folder_and_file_name(
+    folder_name=source_folder_name, file_name=source_file_name)
+source_file_name_match_type = args.source_file_name_match_type
 
 
 project_id = os.environ.get('SHIPYARD_PROJECT_ID')
 vessel_id = os.environ.get('SHIPYARD_VESSEL_ID')
 log_id = os.environ.get('SHIPYARD_LOG_ID')
-
 shipyard_link = create_shipyard_link(
     project_id=project_id, vessel_id=vessel_id, log_id=log_id)
 
@@ -205,9 +281,15 @@ if destination_type == 'dm':
         message_response = send_slack_message(
             slack_connection, message, user_id, create_blocks(message, shipyard_link))
         if source_file_name:
+            file_to_upload = determine_file_to_upload(
+                source_file_name_match_type, source_folder_name, source_file_name)
             channel_id, timestamp = get_message_details(message_response)
             file_response = upload_file_to_slack(
-                slack_connection, file_name=source_file_name, channel=user_id, timestamp=timestamp)
+                slack_connection, file_name=file_to_upload, channel_name=user_id, timestamp=timestamp)
+            file_timestamp, download_link = get_file_download_details(
+                file_response)
+            update_slack_message(slack_connection, message, channel_id=channel_id, blocks=create_blocks(
+                message, download_link=download_link, shipyard_link=shipyard_link), timestamp=timestamp)
 
 else:
     names_to_tag = create_name_tags(user_id_list)
@@ -216,14 +298,15 @@ else:
     message_response = send_slack_message(
         slack_connection, message, channel_name, create_blocks(message, shipyard_link))
     if source_file_name:
+        file_to_upload = determine_file_to_upload(
+            source_file_name_match_type, source_folder_name, source_file_name)
         channel_id, timestamp = get_message_details(message_response)
         file_response = upload_file_to_slack(
-            slack_connection, file_name=source_file_name, channel=channel_name, timestamp=timestamp)
-
-
-file_timestamp, download_link = get_file_download_details(file_response)
-update_slack_message(slack_connection, message, channel=channel_id, blocks=create_blocks(
-    message, download_link=download_link, shipyard_link=shipyard_link), timestamp=timestamp)
+            slack_connection, file_name=file_to_upload, channel=channel_name, timestamp=timestamp)
+        file_timestamp, download_link = get_file_download_details(
+            file_response)
+        update_slack_message(slack_connection, message, channel_id=channel_id, blocks=create_blocks(
+            message, download_link=download_link, shipyard_link=shipyard_link), timestamp=timestamp)
 
 
 # code.interact(local=locals())
