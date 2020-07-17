bef08ffab8e5c2439d0d14c064cc2c52ad104ed6
Luka Damjanovic <luka@thedifferenceinvestments.com>
2020-07-17 15:12:44 -0400

    athena: adding support for executing queries

---
+++ database/athena/execute_sql.py
@@ -0,0 +1,69 @@
+import time
+import argparse
+
+import boto3
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--access-key', dest='access_key', required=True)
+    parser.add_argument('--secret-key', dest='secret_key', required=False)
+    parser.add_argument('--region-name', dest='region_name', required=True)
+    parser.add_argument('--bucket', dest='bucket', required=True)
+    parser.add_argument('--database', dest='database', required=True)
+    parser.add_argument('--query', dest='query', required=True)
+    args = parser.parse_args()
+    return args
+
+
+def poll_status(client, job_id):
+    '''
+    poll query status
+    '''
+    result = client.get_query_execution(
+        QueryExecutionId = job_id
+    )
+
+    state = result['QueryExecution']['Status']['State']
+    if state == 'SUCCEEDED':
+        print(f'Query completed')
+        return result
+    elif state == 'FAILED':
+        print(f'Query failed')
+        return result
+    return False
+
+
+def main():
+    args = get_args()
+    access_key = args.access_key
+    secret_key = args.secret_key
+    region_name = args.region_name
+    database = args.database
+    bucket = args.bucket
+    query = args.query
+
+    try:
+        client = boto3.client('athena', region_name=region_name,
+                aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    except Exception as e:
+        print(f'Failed to access Athena with specified credentials')
+        raise(e)
+
+    job = client.start_query_execution(
+                QueryString=query,
+                QueryExecutionContext={'Database': database},
+                ResultConfiguration={'OutputLocation': bucket}
+                )
+
+    job_id = job['QueryExecutionId']
+
+    while not poll_status(client, job_id):
+        time.sleep(0.1)
+        print(f'Polling query status')
+
+    print('Your query has been successfully executed.')
+
+
+if __name__ == '__main__':
+    main()
