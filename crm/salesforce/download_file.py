eb958da09416436029b59af0e03e55e59fcabdde
Blake Burch <blake@shipyardapp.com>
2020-07-22 14:31:18 -0500

    Autopep8 aggressive formatting.

--- crm/salesforce/download_file.py
+++ crm/salesforce/download_file.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 from simple_salesforce import Salesforce
 
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--username',
@@ -10,14 +11,21 @@ def get_args():
     parser.add_argument('--password',
                         dest='password', required=True)
     parser.add_argument('--security-token',
-                        dest='security_token', required=True)               
+                        dest='security_token', required=True)
     parser.add_argument('--query',
                         dest='query', required=True)
-    parser.add_argument('--destination-file-name',
-                        dest='destination_file_name', default='output.csv', required=False)
-    parser.add_argument('--destination-folder-name',
-                        dest='destination_folder_name', default='', required=False)
+    parser.add_argument(
+        '--destination-file-name',
+        dest='destination_file_name',
+        default='output.csv',
+        required=False)
+    parser.add_argument(
+        '--destination-folder-name',
+        dest='destination_folder_name',
+        default='',
+        required=False)
     return parser.parse_args()
+
 
 def combine_folder_and_file_name(folder_name, file_name):
     """
@@ -39,6 +47,7 @@ def clean_folder_name(folder_name):
         folder_name = os.path.normpath(folder_name)
     return folder_name
 
+
 def main():
     args = get_args()
     username = args.username
@@ -55,14 +64,15 @@ def main():
         os.makedirs(destination_folder_name)
 
     sf_connection = Salesforce(
-    username=username, 
-    password=password, 
-    security_token=security_token,
-    client_id='Shipyard')
+        username=username,
+        password=password,
+        security_token=security_token,
+        client_id='Shipyard')
 
     sf_data = sf_connection.query_all(query)
     sf_df = pd.DataFrame(sf_data['records']).drop(columns='attributes')
-    sf_df.to_csv(destination_name,index=False)
+    sf_df.to_csv(destination_name, index=False)
+
 
 if __name__ == '__main__':
     main()
