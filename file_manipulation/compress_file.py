620a80e948eb0bd94409d0f5494943614c1d5348
Blake Burch <blake@shipyardapp.com>
2020-07-22 14:35:01 -0500

    Autopep8 aggressive formatting

--- file_manipulation/compress_file.py
+++ file_manipulation/compress_file.py
@@ -5,21 +5,41 @@ import tarfile
 import glob
 import re
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--compression', dest='compression',
-                        choices={'zip','tar','tar.bz2','tar.gz'}, required=True)
-    parser.add_argument('--source-file-name-match-type', dest='source_file_name_match_type',
-                        choices={'exact_match', 'regex_match'}, required=True)
+    parser.add_argument(
+        '--compression',
+        dest='compression',
+        choices={
+            'zip',
+            'tar',
+            'tar.bz2',
+            'tar.gz'},
+        required=True)
+    parser.add_argument(
+        '--source-file-name-match-type',
+        dest='source_file_name_match_type',
+        choices={
+            'exact_match',
+            'regex_match'},
+        required=True)
     parser.add_argument('--source-folder-name',
                         dest='source_folder_name', default='', required=False)
     parser.add_argument('--source-file-name',
                         dest='source_file_name', required=True)
-    parser.add_argument('--destination-file-name',
-                        dest='destination_file_name', default='Archive', required=False)
-    parser.add_argument('--destination-folder-name',
-                        dest='destination_folder_name', default='', required=False)
+    parser.add_argument(
+        '--destination-file-name',
+        dest='destination_file_name',
+        default='Archive',
+        required=False)
+    parser.add_argument(
+        '--destination-folder-name',
+        dest='destination_folder_name',
+        default='',
+        required=False)
     return parser.parse_args()
+
 
 def clean_folder_name(folder_name):
     """
@@ -29,6 +49,7 @@ def clean_folder_name(folder_name):
     if folder_name != '':
         folder_name = os.path.normpath(folder_name)
     return folder_name
+
 
 def combine_folder_and_file_name(folder_name, file_name):
     """
@@ -40,6 +61,7 @@ def combine_folder_and_file_name(folder_name, file_name):
 
     return combined_name
 
+
 def find_all_local_file_names(source_folder_name):
     """
     Returns a list of all files that exist in the current working directory,
@@ -49,6 +71,7 @@ def find_all_local_file_names(source_folder_name):
     cwd_extension = os.path.normpath(f'{cwd}/{source_folder_name}/**')
     file_names = glob.glob(cwd_extension, recursive=True)
     return [file_name for file_name in file_names if os.path.isfile(file_name)]
+
 
 def find_all_file_matches(file_names, file_name_re):
     """
@@ -61,34 +84,36 @@ def find_all_file_matches(file_names, file_name_re):
 
     return matching_file_names
 
-def compress_files(file_paths,destination_full_path,compression):
+
+def compress_files(file_paths, destination_full_path, compression):
     """
     Compress all of the matched files using the specified compression method.
     """
     compressed_file_name = f'{destination_full_path}.{compression}'
     if compression == 'zip':
-        with ZipFile(compressed_file_name,'w') as zip:
+        with ZipFile(compressed_file_name, 'w') as zip:
             for file in file_paths:
-                file = file.replace(os.getcwd(),'')[1:]
+                file = file.replace(os.getcwd(), '')[1:]
                 zip.write(file)
-    
-    if compression =='tar.bz2':
-        with tarfile.open(compressed_file_name,'w:bz2') as tar:
+
+    if compression == 'tar.bz2':
+        with tarfile.open(compressed_file_name, 'w:bz2') as tar:
             for file in file_paths:
-                file = file.replace(os.getcwd(),'')[1:]
+                file = file.replace(os.getcwd(), '')[1:]
                 tar.add(file)
 
-    if compression =='tar':
-        with tarfile.open(compressed_file_name,'w') as tar:
+    if compression == 'tar':
+        with tarfile.open(compressed_file_name, 'w') as tar:
             for file in file_paths:
-                file = file.replace(os.getcwd(),'')[1:]
+                file = file.replace(os.getcwd(), '')[1:]
                 tar.add(file)
 
-    if compression =='tar.gz':
-        with tarfile.open(compressed_file_name,'w:gz') as tar:
+    if compression == 'tar.gz':
+        with tarfile.open(compressed_file_name, 'w:gz') as tar:
             for file in file_paths:
-                file = file.replace(os.getcwd(),'')[1:]
+                file = file.replace(os.getcwd(), '')[1:]
                 tar.add(file)
+
 
 def main():
     args = get_args()
@@ -107,14 +132,17 @@ def main():
         file_paths = find_all_local_file_names(source_folder_name)
         matching_file_paths = find_all_file_matches(
             file_paths, re.compile(source_file_name))
-        print(f'{len(matching_file_paths)} files found. Preparing to compress with {compression}...')
-        if not os.path.exists(destination_folder_name) and (destination_folder_name != ''):
+        print(
+            f'{len(matching_file_paths)} files found. Preparing to compress with {compression}...')
+        if not os.path.exists(destination_folder_name) and (
+                destination_folder_name != ''):
             os.makedirs(destination_folder_name)
-        compress_files(matching_file_paths,destination_full_path,compression)
+        compress_files(matching_file_paths, destination_full_path, compression)
     else:
-        if not os.path.exists(destination_folder_name) and (destination_folder_name != ''):
+        if not os.path.exists(destination_folder_name) and (
+                destination_folder_name != ''):
             os.makedirs(destination_folder_name)
-        compress_files([source_full_path],destination_full_path,compression)
+        compress_files([source_full_path], destination_full_path, compression)
 
 
 if __name__ == '__main__':
--- file_manipulation/decompress_file.py
+++ file_manipulation/decompress_file.py
@@ -5,19 +5,31 @@ import tarfile
 import glob
 import re
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--compression', dest='compression',
-                        choices={'zip','tar','tar.bz2','tar.gz'}, required=True)
+    parser.add_argument(
+        '--compression',
+        dest='compression',
+        choices={
+            'zip',
+            'tar',
+            'tar.bz2',
+            'tar.gz'},
+        required=True)
     parser.add_argument('--source-folder-name',
                         dest='source_folder_name', default='', required=False)
     parser.add_argument('--source-file-name',
                         dest='source_file_name', required=True)
     parser.add_argument('--destination-file-name',
                         dest='destination_file_name', required=False)
-    parser.add_argument('--destination-folder-name',
-                        dest='destination_folder_name', default='', required=False)
+    parser.add_argument(
+        '--destination-folder-name',
+        dest='destination_folder_name',
+        default='',
+        required=False)
     return parser.parse_args()
+
 
 def clean_folder_name(folder_name):
     """
@@ -27,6 +39,7 @@ def clean_folder_name(folder_name):
     if folder_name != '':
         folder_name = os.path.normpath(folder_name)
     return folder_name
+
 
 def combine_folder_and_file_name(folder_name, file_name):
     """
@@ -38,35 +51,38 @@ def combine_folder_and_file_name(folder_name, file_name):
 
     return combined_name
 
-def decompress_file(source_full_path,destination_full_path,compression):
+
+def decompress_file(source_full_path, destination_full_path, compression):
     """
     Decompress a given file, using the specified compression method.
     """
-    
+
     compressed_file_name = f'{destination_full_path}.{compression}'
     if compression == 'zip':
         with ZipFile(source_full_path, 'r') as zip:
             zip.extractall(destination_full_path)
-                
-    if compression =='tar.bz2':
-        file = tarfile.open(source_full_path,'r:bz2')
+
+    if compression == 'tar.bz2':
+        file = tarfile.open(source_full_path, 'r:bz2')
         file.extractall(path=destination_full_path)
 
-    if compression =='tar':
-        file = tarfile.open(source_full_path,'r')
+    if compression == 'tar':
+        file = tarfile.open(source_full_path, 'r')
         file.extractall(path=destination_full_path)
 
-    if compression =='tar.gz':
-        file = tarfile.open(source_full_path,'r:gz')
+    if compression == 'tar.gz':
+        file = tarfile.open(source_full_path, 'r:gz')
         file.extractall(path=destination_full_path)
 
-def create_fallback_destination_file_name(source_file_name,compression):
+
+def create_fallback_destination_file_name(source_file_name, compression):
     """
     If a destination_file_name is not provided, uses the source_file_name with a removal of the compression extension.
     """
     file_name = os.path.basename(source_file_name)
-    file_name = file_name.replace(f'.{compression}','')
+    file_name = file_name.replace(f'.{compression}', '')
     return file_name
+
 
 def main():
     args = get_args()
@@ -79,13 +95,16 @@ def main():
     destination_file_name = args.destination_file_name
 
     if destination_file_name is None:
-        destination_file_name = create_fallback_destination_file_name(source_file_name,compression)
+        destination_file_name = create_fallback_destination_file_name(
+            source_file_name, compression)
 
-    destination_full_path = combine_folder_and_file_name(destination_folder_name, destination_file_name)
+    destination_full_path = combine_folder_and_file_name(
+        destination_folder_name, destination_file_name)
 
-    if not os.path.exists(destination_folder_name) and (destination_folder_name != ''):
+    if not os.path.exists(destination_folder_name) and (
+            destination_folder_name != ''):
         os.makedirs(destination_folder_name)
-    decompress_file(source_full_path,destination_full_path,compression)
+    decompress_file(source_full_path, destination_full_path, compression)
 
 
 if __name__ == '__main__':
